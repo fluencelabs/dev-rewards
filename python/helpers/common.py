1d89f6f2eeebd8d1fcc06a2f057ca9d463d0709f
51bitquant <stephen0823@aliyun.com>
2024-03-03 09:37:23 +1030

    fix TypeError: 'type' object is not subscriptable

--- python/helpers/common.py
+++ python/helpers/common.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
 from dataclasses_json import dataclass_json
+from typing import List, Dict
 
 
 @dataclass_json
 @dataclass
 class Metadata:
     root: str
-    addresses: list[str]
-    encryptedKeys: dict[str, dict[str, str]]
+    addresses: List[str]
+    encryptedKeys: Dict[str, Dict[str, str]]
+
